fffd8bf681fb07d3c6088dfc8971dcc45b894008
Arjun013H <csarjun11@gmail.com>
2023-06-03 12:11:35 +0530

    initial commit

---
+++ hyperwall.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+import os
+import json
+import sys
+
+loc = "/home/arjun/arjun/apps/hyperwalset"
+
+
+def hyperset(c1, wall):
+    file = open(f"{loc}/config/hyper/hyprland.conf", "r")
+    data = file.read()
+    file.close()
+    data = data.replace("{insertCol}", c1.strip("#"))
+    data = data.replace("{wallpaperset}", wall)
+    file = open("/home/arjun/.config/hypr/hyprland.conf", "w")
+    file.write(data)
+    file.close()
+
+
+def waybar(c1, c2):
+    for i in ["config", "style.css"]:
+        file = open(f"{loc}/config/waybar/{i}", "r")
+        data = file.read()
+        file.close()
+
+        data = data.replace("{insertCol1}", c1)
+        data = data.replace("{insertCol2}", c2)
+
+        file = open(f"/home/arjun/.config/waybar/{i}", "w")
+        file.write(data)
+
+        # os.system('killall waybar')
+        # os.system('waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css')
+        #
+
+
+if __name__ == "__main__":
+    os.system("rm /home/arjun/.cache/wal/schemes/*")
+    os.system(f"swaybg -m fill -i {sys.argv[1]} & wal -i {sys.argv[1]}")
+
+    dirs = os.listdir("/home/arjun/.cache/wal/schemes/")
+    jsfile = dirs[0]
+
+    file = open(f"/home/arjun/.cache/wal/schemes/{jsfile}")
+
+    data = json.load(file)
+    colors = data["colors"]
+    c1, c2 = colors["color1"], colors["color15"]
+    hyperset(c1, data["wallpaper"])
+    waybar(c1, c2)
