6b5a43f0ffe9ddf258f488ae688b37f8f8552e46
Arjun013H <csarjun11@gmail.com>
2023-07-01 22:27:30 +0530

    added a user variable

--- hyperwall.py
+++ hyperwall.py
@@ -2,9 +2,9 @@
 import os
 import json
 import sys
-
+username='arjun'
 #location of the repo
-loc = "/home/arjun/arjun/apps/hyperwalset"
+loc = f"/home/{username}/arjun/apps/hyperwalset"
 
 
 def hyperset(c1, wall):
@@ -13,7 +13,7 @@ def hyperset(c1, wall):
     file.close()
     data = data.replace("{insertCol}", c1.strip("#"))
     data = data.replace("{wallpaperset}", wall)
-    file = open("/home/arjun/.config/hypr/hyprland.conf", "w")
+    file = open(f"/home/{username}/.config/hypr/hyprland.conf", "w")
     file.write(data)
     file.close()
 
@@ -27,7 +27,7 @@ def waybar(c1, c2):
         data = data.replace("{insertCol1}", c1)
         data = data.replace("{insertCol2}", c2)
 
-        file = open(f"/home/arjun/.config/waybar/{i}", "w")
+        file = open(f"/home/{username}/.config/waybar/{i}", "w")
         file.write(data)
 
         # os.system('killall waybar')
@@ -36,18 +36,17 @@ def waybar(c1, c2):
 
 
 if __name__ == "__main__":
-    os.system("rm /home/arjun/.cache/wal/schemes/*")
+    os.system(f"rm /home/{username}/.cache/wal/schemes/*")
     os.system(f"swaybg -m fill -i {sys.argv[1]} & wal -i {sys.argv[1]}")
 
-    dirs = os.listdir("/home/arjun/.cache/wal/schemes/")
+    dirs = os.listdir(f"/home/{username}/.cache/wal/schemes/")
     jsfile = dirs[0]
 
-    file = open(f"/home/arjun/.cache/wal/schemes/{jsfile}")
+    file = open(f"/home/{username}/.cache/wal/schemes/{jsfile}")
 
     data = json.load(file)
     colors = data["colors"]
     c1, c2 = colors["color1"], colors["color15"]
-    walldata = "~/arjun/"+data["wallpaper"].strip('/home/arjun/')
+    walldata = f"~/{username}/"+data["wallpaper"].strip(f'/home/{username}/')
     hyperset(c1, walldata)
     waybar(c1, c2)
-    print(walldata)
